6fc58d218b95ce3b97397cbb4dabec5c966d66c6
flaviaprouvot <flavia.prouvot@gmail.com>
2025-01-31 15:47:07 -0300

    atividades aula-01

--- Javascript/atividades-aula-01/script6.js
+++ Javascript/atividades-aula-01/script6.js
@@ -1,9 +1,10 @@
 /*Escreva em JavaScript um programa que informe o retorno de um investimento (montante) com base nos valores do capital inicial,
-tempo em meses e taxa de juros mensal, fornecidos pelo usuário. Use a fórmula: M = C * (1+i) t
+tempo em meses e taxa de juros mensal, fornecidos pelo usuário. Use a fórmula: M = C * (1+i)** t
 */
 
-var n1 = Number.parseFloat  (prompt("Digite seu capital inicial: "));
-var n2 = Number.parseFloat  (prompt("Digite a taxa de juros, em percentual: "));
-var n3 = Number.parseFloat  (prompt("Digite seu tempo do investimento, em meses: "));
-var resultadoa = (n1 * (1+n2)**n3);
+var n1 = Number.parseInt  (prompt("Digite seu capital inicial: "));
+var n2 = Number.parseInt  (prompt("Digite a taxa de juros, em percentual: "));
+var n3 = Number.parseInt  (prompt("Digite seu tempo do investimento, em meses: "));
+var resultado = (n1 * ((1 + (n2/ 100)) ** n3));
 alert(resultado.toFixed(2));
+
