bf59321d0c40121409900da106732712be41cfe6
flaviaprouvot <flavia.prouvot@gmail.com>
2025-01-31 12:27:27 -0300

    aula-01

---
+++ Javascript/aula-01/README.md
@@ -0,0 +1,6 @@
+Aula introdutoria sobre Lógica de programação com JavaScript
+
+-Declaração de variáveis
+-Concatenação
+-Conversão
+-Alert/Prompt
---
+++ Javascript/aula-01/script.js
@@ -0,0 +1,23 @@
+console.log("ola mundo");
+
+//exemplo de alert()
+alert("ola mundo 1");
+
+//exemplo de confirm()
+var teste = confirm("ola mundo 2");
+    //exibindo opcao escolhina pelo usuário
+    console.log(teste)
+
+//exemplo de prompt()
+var testePrompt = prompt("digite seu nome");
+    //exibindo nome do usuario
+    console.log(testePrompt);
+    alert(testePrompt);
+
+//tentativa de adição
+var numero1 = Number.parseInt  (prompt("Digite o primeiro número"));
+var numero2 = Number.parseInt (prompt("Digite o segundo número"));
+
+//verficando o tipo da variável 
+console.log(typeof(numero1));
+alert(numero1 + numero2);
